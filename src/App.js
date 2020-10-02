8607b49a1e70ff95db6b0fe27102d1b5a403d24d
Sharmik Hirpara <sharmik.hirpara@gmail.com>
2020-10-02 12:20:33 +1000

    Added Decrement feature

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ class App extends Component {
       { id: 1, value: 0 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
+      { id: 4, value: 0 },
+    ],
   };
 
   constructor() {
@@ -22,7 +22,7 @@ class App extends Component {
     console.log("App - Mounted");
   }
 
-  handleIncrement = counter => {
+  handleIncrement = (counter) => {
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };
@@ -30,16 +30,24 @@ class App extends Component {
     this.setState({ counters });
   };
 
+  handleDecrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
+
   handleReset = () => {
-    const counters = this.state.counters.map(c => {
+    const counters = this.state.counters.map((c) => {
       c.value = 0;
       return c;
     });
     this.setState({ counters });
   };
 
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
     this.setState({ counters });
   };
 
@@ -48,13 +56,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <NavBar
-          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
         />
         <main className="container">
           <Counters
             counters={this.state.counters}
             onReset={this.handleReset}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
             onDelete={this.handleDelete}
           />
         </main>
